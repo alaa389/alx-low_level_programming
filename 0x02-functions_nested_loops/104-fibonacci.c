25ad1a4e59a3f04db22fe5ff8ac5723b31ad270d
alaa389 <alaai2468567@gmail.com>
2023-10-15 17:26:07 -0700

    ffsd

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,13 +1,32 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
+
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* main - prints the numbers from 1 to 100
+*
+* Return: Always success.
+*/
+
+int main(void)
 {
-	return (write(1, &c, 1));
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+	{
+		if (i > 1)
+			putchar(' ');
+
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
+		else if (i % 5 == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", i);
+	}
+putchar('\n');
+return (0);
 }
